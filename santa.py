d9f7f5b436398566f55be67750249b10707b7246
NikaMdinaradze <75743331+NikaMdinaradze@users.noreply.github.com>
2022-12-06 09:43:26 -0800

    Update santa.py

--- santa.py
+++ santa.py
@@ -1,5 +1,10 @@
 import random
-from os import system, name
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+email_sender = ''#enter your email
+email_password = '' #enter your email password (16 digits)
 
 mimgebebi = []
 mchuqnelebi = []
@@ -22,10 +27,16 @@ for mchuqneli in mchuqnelebi:
         random_mimgebi = random.choice(mimgebebi)
     mimgebebi.remove(random_mimgebi)
     print(mchuqneli)
-    input()
     print(random_mimgebi)
-    input()
-    system('cls')
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = mchuqneli
+    em['Subject'] = "sekret santai"
+    em.set_content(random_mimgebi)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, mchuqneli, em.as_string())
 
 
     
