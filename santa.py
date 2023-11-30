cf664b2952dedb1520f9029cafcc509fbb2ef30e
NikaMdinaradze <75743331+NikaMdinaradze@users.noreply.github.com>
2023-11-30 17:21:34 +0400

    There will be bugs. I wrote this without run.

--- santa.py
+++ santa.py
@@ -6,40 +6,52 @@ from email.message import EmailMessage
 email_sender = ''#enter your email
 email_password = '' #enter your email password (16 digits)
 
-mimgebebi = []
-mchuqnelebi = []
+
+def get_people():
+    people = []
+    name = 1
+    print("ENTER GMAILS")
+    while name:
+        name = input("->")
+        if name in people:
+            print("gmail is already in list")
+            continue
+        people.append(name)
+    people.pop()
+    return people
 
 
-saxeli = "saxeli"
-while saxeli != "":
-    saxeli = input("chaswerei saxeli: ")
-    mimgebebi.append(saxeli)
-    mchuqnelebi.append(saxeli)
-
-mimgebebi.pop()
-mchuqnelebi.pop()
+def check_self_senders(list1, list2):
+    zipped = zip(list1, list2)
+    for person1, person2 in zipped:
+        if person1 == person2:
+            return True
+    return False
 
 
+def pair_people(people):
+    senders = [*people]
+    random.shuffle(people)
+    while check_self_senders(senders, people):
+        random.shuffle(people)
 
-for mchuqneli in mchuqnelebi:
-    random_mimgebi = random.choice(mimgebebi)
-    while random_mimgebi == mchuqneli:
-        random_mimgebi = random.choice(mimgebebi)
-    mimgebebi.remove(random_mimgebi)
-    print(mchuqneli)
-    print(random_mimgebi)
+    return zip(senders, people)
+
+
+def send_mail(sender, receiver):
     em = EmailMessage()
     em['From'] = email_sender
-    em['To'] = mchuqneli
-    em['Subject'] = "sekret santai"
-    em.set_content(random_mimgebi)
+    em['To'] = sender
+    em['Subject'] = "Secret Santa"
+    em.set_content(f"You are secret santa of {receiver}")
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, mchuqneli, em.as_string())
+        smtp.sendmail(email_sender, sender, em.as_string())
 
 
-    
-    
+people = get_people()
+paired = pair_people(people)
 
-
+for sender, receiver in paired:
+    send_mail(sender, receiver)
